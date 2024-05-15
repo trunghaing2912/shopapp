fa8e8baa897f8169826afb682646549484f8d93b
Hai Nguyen Trung (VTI.DG) <hai.nguyentrung@vti.com.vn>
2024-05-15 18:42:32 +0700

    third commit

--- shopapp-angular/src/app/app.module.ts
+++ shopapp-angular/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [
-    HomeComponent
+    // HomeComponent,
+    // OrderComponent,
+    // OrderConfirmComponent,
+    // LoginComponent,
+    // RegisterComponent,
+    DetailProductComponent
   ]
 })
 export class AppModule { }
