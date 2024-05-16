d2412a9d87ec2bdda8559ab0b97d55bed41a3982
Hai Nguyen Trung (VTI.DG) <hai.nguyentrung@vti.com.vn>
2024-05-16 18:43:35 +0700

    backend commit

---
+++ shopapp-backend/shopapp/src/main/java/com/project/shopapp/dtos/CategoryDTO.java
@@ -0,0 +1,18 @@
+package com.project.shopapp.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+
+    @NotEmpty(message = "Category's name cannot be empty")
+    private String name;
+
+
+
+}
