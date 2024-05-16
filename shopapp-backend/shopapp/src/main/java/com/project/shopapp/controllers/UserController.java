db0e32eb85bcbed838d29fad0ccbf06d956693b2
Trung Hai Nguyen <trunghaing2912@gmail.com>
2024-05-17 00:45:00 +0700

    commit from home

---
+++ shopapp-backend/shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -0,0 +1,42 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.UserDTO;
+import com.project.shopapp.dtos.UserLoginDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> crreatUser(@Valid @RequestBody UserDTO userDTO,
+                                        BindingResult result) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            return ResponseEntity.ok("Register successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> crreatUser(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+        return ResponseEntity.ok("some token");
+    }
+}
