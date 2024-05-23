b05f9990da59c23d2f1f44cefa0218c8a0febe7b
Hai Nguyen Trung (VTI.DG) <hai.nguyentrung@vti.com.vn>
2024-05-23 17:45:55 +0700

    update FE

--- shopapp-angular/src/app/app.module.ts
+++ shopapp-angular/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
     DetailProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [
@@ -30,8 +32,8 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
     // OrderComponent,
     // OrderConfirmComponent,
     // LoginComponent,
-    // RegisterComponent,
-    DetailProductComponent
+    RegisterComponent,
+    // DetailProductComponent
   ]
 })
 export class AppModule { }
--- shopapp-angular/src/app/register/register.component.ts
+++ shopapp-angular/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
+  // Khai báo các biến tương ứng với các trường dữ liệu trong form
+  phoneNumber: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  isAccepted: boolean;
+  dateOfBirth: Date;
 
+  constructor(private http: HttpClient, private router: Router) {
+    this.phoneNumber = '';
+    this.password = '';
+    this.retypePassword = '';
+    this.fullName = '';
+    this.address = '';
+    this.isAccepted = true;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+    //inject
+  }
+
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phoneNumber}`)
+  }
+
+  register() {
+    const message = `phone: ${this.phoneNumber} ` +
+      `password: ${this.password} ` +
+      `retypePassword: ${this.retypePassword} ` +
+      `address: ${this.address} ` +
+      `fullName: ${this.fullName} ` +
+      `isAccpeted: ${this.isAccepted}`;
+    // alert(message)
+    const apiUrl = 'users/registerr'
+  }
+
+  //check password match
+  checkPasswordsMatch() {
+    if (this.password !== this.retypePassword) {
+      this.registerForm.form.controls['retypePassword'].setErrors({ 'passwordMismatch': true });
+    } else {
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({ 'invalidAge': true })
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
+  }
 }
